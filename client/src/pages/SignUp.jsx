0e9c8ab2ad544a9403ada6b2b5950a76752c79e2
Cessaprince <cessaprince2007@gmail.com>
2026-08-11 14:03:10 +0100

    touched the signin page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import { Eye, EyeOff } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import googleLogo from '../images/page-images/google-logo.png'
-
+import iziToast from 'izitoast'
 
 const SignUp = () => {
 
@@ -54,10 +54,30 @@ const SignUp = () => {
     const errorMessageConfirm = (isTypingConfirm === true) && (password !== confirmPassword)
 
 
-    //the button
+    //the button function
+    const [isLoading, setIsLoading] = useState(false);
 
-    
+    const buttonClicked = (e) => {
 
+        e.preventDefault()
+
+        if (isLoading === true) {
+            return
+        }
+
+        setIsLoading(true)
+
+        setTimeout(() => {
+            setIsLoading(false)
+
+            iziToast.success({
+                title: 'Success!',
+                message: 'Sign up Successful!',
+                position: 'topRight',
+                timeout: 3000,
+            })
+        }, 500)
+    }
 
     return (
         <div>
@@ -66,9 +86,18 @@ const SignUp = () => {
                     <div className=''>
                         <form action="" className='w-full flex flex-col gap-[15px] '>
                             <h1 className='font-bold text-[22px] text-center'>SignUp</h1>
-                            <input type="text" placeholder='Username' className='text-[15px] border-[0.1px] border-gray-400 shadow-sm bg-white py-[10px] px-[15px] text-gray-500 w-full outline-none rounded-[5px]' />
 
-                            <input type="email" placeholder='Email Address' className='text-[15px] border-[0.1px] border-gray-400 shadow-sm bg-white py-[10px] px-[15px] text-gray-500 w-full outline-none rounded-[5px]' />
+                            <input
+                                type="text"
+                                placeholder='Username'
+                                className='text-[15px] border-[0.1px] border-gray-400 shadow-sm bg-white py-[10px] px-[15px] text-gray-500 w-full outline-none rounded-[5px]'
+                            />
+
+                            <input
+                                type="email"
+                                placeholder='Email Address'
+                                className='text-[15px] border-[0.1px] border-gray-400 shadow-sm bg-white py-[10px] px-[15px] text-gray-500 w-full outline-none rounded-[5px]'
+                            />
 
                             <div className="flex flex-col gap-[5px]">
                                 <div className="password text-[15px] flex justify-between border-[0.1px] border-gray-400 shadow-sm items-center bg-white py-[10px] px-[15px] text-gray-500 w-full outline-none rounded-[5px]">
@@ -78,8 +107,8 @@ const SignUp = () => {
                                         className='w-[80%] h-full outline-none'
                                         value={password}
                                         onChange={checkPassword}
-
                                     />
+
                                     <span className='cursor-pointer' onClick={toggleCreatePassword}>
                                         {seeCreatePassword ? <Eye size={18} /> : <EyeOff size={18} />}
                                     </span>
@@ -101,6 +130,7 @@ const SignUp = () => {
                                         value={confirmPassword}
                                         onChange={checkComfirmPassword}
                                     />
+
                                     <span className='cursor-pointer' onClick={toggleConfirmPassword}>
                                         {seeConfirmPassword ? <Eye size={18} /> : <EyeOff size={18} />}
                                     </span>
@@ -114,22 +144,38 @@ const SignUp = () => {
                             </div>
 
 
+                            <button
+                                type="submit"
+                                className={`${isLoading? 'bg-[var(--royalblue-hover)]': 'bg-[var(--royalblue)]'} w-full py-[10px] font-light text-white rounded-[5px] hover:bg-[var(--royalblue-hover)] hover:-translate-y-1 cursor-pointer transition-all duration-300 ease`}
+                                onClick={buttonClicked}
+                                disabled={isLoading}
+                            >
+                                {isLoading ? 'Signing up..' : 'Submit'}
+                            </button>
 
-                            <button type="submit" className='w-full py-[10px] bg-[var(--royalblue)] font-light text-white rounded-[5px] hover:bg-[var(--royalblue-hover)] hover:-translate-y-1 cursor-pointer transition-all duration-300 ease'>Signup</button>
-                            <p className='text-[15px] text-center text-gray-500'>Already have an account? <Link to='/login' className='text-[var(--royalblue)] hover:text-[var(--royalblue-hover)]'>Login</Link></p>
+                            <p className='text-[15px] text-center text-gray-500'>
+                                Already have an account?
+                                <Link to='/login' className='text-[var(--royalblue)] hover:text-[var(--royalblue-hover)]'>
+                                    Login
+                                </Link>
+                            </p>
 
                             <fieldset className="border-t border-gray-300 my-[5px] text-center">
                                 <legend className="px-4 text-sm font-medium text-gray-500 uppercase">
                                     or
                                 </legend>
                             </fieldset>
-                            <button type="submit" className='group w-full py-[10px] border-[0.1px] border-gray-400 font-light text-gray-500 rounded-[5px] hover:-translate-y-1 cursor-pointer transition-all duration-300 ease flex items-center justify-center'>
+
+                            <button
+                                type="button"
+                                className='group w-full py-[10px] border-[0.1px] border-gray-400 font-light text-gray-500 rounded-[5px] hover:-translate-y-1 cursor-pointer transition-all duration-300 ease flex items-center justify-center'
+                            >
                                 <span className='w-[50px] h-full rounded-full overflow-hidden'>
                                     <img src={googleLogo} alt="" />
                                 </span>
+
                                 <span className='text-[15px]'>Signup with Google</span>
                             </button>
-
 
                         </form>
 
@@ -141,3 +187,4 @@ const SignUp = () => {
 }
 
 export default SignUp
+
